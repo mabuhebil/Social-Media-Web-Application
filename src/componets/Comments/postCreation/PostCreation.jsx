4bff3dadcc06eafa1a47e684c6b2f945cda6b840
Mohammed Abu Hebil <mabuhebil@gmail.com>
2026-04-20 14:24:40 +0200

    update

--- src/componets/Comments/postCreation/PostCreation.jsx
+++ src/componets/Comments/postCreation/PostCreation.jsx
@@ -1,8 +1,31 @@
-import { Avatar, Card, CardBody } from "@heroui/react";
-import React from "react";
-import { Rocket } from "@gravity-ui/icons";
-import { Button, Modal } from "@heroui/react";
+import { Avatar, Card, CardBody, Textarea } from "@heroui/react";
+import { CiImageOn } from "react-icons/ci";
+import { IoCloseCircleOutline } from "react-icons/io5";
+import React, { useRef, useState } from "react";
+import {
+  Modal,
+  ModalContent,
+  ModalHeader,
+  ModalBody,
+  ModalFooter,
+  Button,
+  useDisclosure,
+} from "@heroui/react";
 export default function PostCreation() {
+  const { isOpen, onOpen, onOpenChange } = useDisclosure();
+  const [isExicedImage, setISExicedImage] = useState(null);
+
+  const ImageInput = useRef();
+  const TextArea = useRef();
+
+  function handelChangeFile(e) {
+    setISExicedImage(URL.createObjectURL(e.target.files[0]));
+  }
+
+  function handleCloseImage() {
+    setISExicedImage(null);
+    ImageInput.current.value = "";
+  }
   return (
     <>
       <section className="pt-12 ">
@@ -13,42 +36,66 @@ export default function PostCreation() {
                 alt="John Doe"
                 src="https://img.heroui.chat/image/avatar?w=400&h=400&u=3"
               />
-              <div>
-                <p>what is in your mind mohamed</p>
+              <div
+                onClick={onOpen}
+                className="bg-gray-600 p-2 rounded-full px-5 w-full"
+              >
+                <p className="text-white">what is in your mind mohamed</p>
               </div>
             </CardBody>
           </Card>
+          <Modal isOpen={isOpen} onOpenChange={onOpenChange}>
+            <ModalContent>
+              {(onClose) => (
+                <>
+                  <ModalHeader className="flex flex-col gap-1">
+                    Create Post
+                  </ModalHeader>
+                  <ModalBody>
+                    <div className="flex flex-row items-center gap-5">
+                      <Avatar alt="John Doe" src={isExicedImage} />
+                      <p>mohamed</p>
+                    </div>
+                    <Textarea
+                      ref={TextArea}
+                      placeholder="whats on your mind , mohamed"
+                    />
 
-          <Modal>
-            <Button variant="secondary" className="text-9xl bg-amber-700">
-              Open Modal
-            </Button>
-            <Modal.Backdrop>
-              <Modal.Container>
-                <Modal.Dialog className="sm:max-w-[360px]">
-                  <Modal.CloseTrigger />
-                  <Modal.Header>
-                    <Modal.Icon className="bg-default text-foreground">
-                      <Rocket className="size-5" />
-                    </Modal.Icon>
-                    <Modal.Heading>Welcome to HeroUI</Modal.Heading>
-                  </Modal.Header>
-                  <Modal.Body>
-                    <p>
-                      A beautiful, fast, and modern React UI library for
-                      building accessible and customizable web applications with
-                      ease.
-                    </p>
-                  </Modal.Body>
-                  <Modal.Footer>
-                    <Button className="w-full" slot="close">
-                      Continue
+                    {isExicedImage && (
+                      <div className="relative">
+                        <img
+                          src={isExicedImage}
+                          alt="hero ui img"
+                          className="rounded-xl"
+                        />
+                        <IoCloseCircleOutline
+                          className="absolute top-3 text-3xl text-white end-3"
+                          onClick={handleCloseImage}
+                        />
+                      </div>
+                    )}
+                  </ModalBody>
+                  <ModalFooter className="flex items-center">
+                    <label>
+                      <CiImageOn className="text-2xl" />
+                      <input
+                        ref={ImageInput}
+                        type="file"
+                        hidden
+                        onChange={handelChangeFile}
+                      />
+                    </label>
+                    <Button color="danger" variant="light" onPress={onClose}>
+                      Close
                     </Button>
-                  </Modal.Footer>
-                </Modal.Dialog>
-              </Modal.Container>
-            </Modal.Backdrop>
-          </Modal>
+                    <Button color="primary" onPress={onClose}>
+                      Action
+                    </Button>
+                  </ModalFooter>
+                </>
+              )}
+            </ModalContent>
+          </Modal>{" "}
         </div>
       </section>
     </>
